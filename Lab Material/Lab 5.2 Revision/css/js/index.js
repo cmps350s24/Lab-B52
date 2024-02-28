d6204697fd078bd423858b3e4df2c41ad9953f63
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-02-28 12:01:58 +0300

    uo

---
+++ Lab Material/Lab 5.2 Revision/css/js/index.js
@@ -0,0 +1,56 @@
+// console.log("hello")
+
+// const PI = 3.14
+// let height = 1.67
+
+// console.log(`PI is ${PI} and height is ${height}`)
+
+// let num = [1, 2, 3, -5, 12, 11, 10]
+// console.log(num);
+// console.log(num);
+
+// // you can use array methods [sort, search , filter , map [covert to other shape], reduce ]
+
+// // array destructing
+// const [first, second, ...others] = num
+// console.log(first, second, others);
+// // do no write anything below this [only for explanation]
+
+// const person = {
+//     name: "Ali",
+//     age: 21,
+//     gender: "male"
+// }
+
+// person.nationality = 'Qatari'
+// person['address'] = 'Dukhan'
+
+// console.log(`The name of the person is ${person.name}`);
+// console.log(person);
+
+
+// array of people using the same person
+const people = [
+    {
+        name: "Ali",
+        age: 21,
+        gender: "male"
+    },
+    {
+        name: "Alanoud",
+        age: 20,
+        gender: "female"
+    },
+    {
+        name: "Omar",
+        age: 23,
+        gender: "male"
+    },
+    {
+        name: "Aisha",
+        age: 19,
+        gender: "female"
+    }
+]
+
+

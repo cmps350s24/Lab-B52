ae2c3583a69f3dfc51de2ef9100f0282568c6f58
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-06 10:07:49 +0300

    updated

--- Lab Material/Lab 6- JS OOP/Tutorial/PartA-B/app.js
+++ Lab Material/Lab 6- JS OOP/Tutorial/PartA-B/app.js
@@ -1,1 +1,11 @@
-console.log('Everything works');
+import { sayHello, getAccounts, addAccount } from "./repo/bank.js";
+
+sayHello('Fatima')
+
+const testAccount = {
+    accountNo: 555,
+    balance: 500,
+    type: 'saving'
+}
+addAccount(testAccount)
+console.log(getAccounts());
---
+++ Lab Material/Lab 6- JS OOP/Tutorial/PartA-B/repo/bank.js
@@ -0,0 +1,24 @@
+export function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+
+const accounts = [
+    {
+        accountNo: 123,
+        balance: 500,
+        type: 'saving'
+    },
+    {
+        accountNo: 234,
+        balance: 4000,
+        type: 'current'
+    }
+]
+
+export function getAccounts() {
+    return accounts
+}
+
+export function addAccount(account) {
+    accounts.push(account)
+}

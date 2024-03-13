8ce706a3492a9bf00b9a9a2777312cab20cfc027
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-13 11:16:49 +0300

    update

--- Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -3,13 +3,19 @@ const countryURL = 'https://restcountries.com/v3.1/name/'
 
 // Get the reference of the element we want to interact with
 const regionDD = document.querySelector('#region')
+const countryDD = document.querySelector('#country')
 
 // add event listener to the selected element
 
 regionDD.addEventListener('change', handleRegionChange)
 
-function handleRegionChange() {
-    const url = regionURL + regionDD.value
-    const data = fetch(url)
-    console.log(data);
+async function handleRegionChange() {
+    const url = `${regionURL}${regionDD.value}`  //construct the proper URL Route to get the data [endpoint]
+    const data = await fetch(url) //download the data
+    const countries = await data.json()  //parse the data
+    const countriesOptions = countries
+        .map(country => `<option value = ${country.name.common}> ${country.name.common}</option>`)
+        .join(' ')
+
+    countryDD.innerHTML = countriesOptions
 }

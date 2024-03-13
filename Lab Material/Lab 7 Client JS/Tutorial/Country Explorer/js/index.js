08ffc9d7e23656c59fdeb3cdca05a5be5e41b357
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-13 11:50:51 +0300

    up

--- Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -4,18 +4,38 @@ const countryURL = 'https://restcountries.com/v3.1/name/'
 // Get the reference of the element we want to interact with
 const regionDD = document.querySelector('#region')
 const countryDD = document.querySelector('#country')
+const factsArea = document.querySelector('#facts-area')
 
 // add event listener to the selected element
 
 regionDD.addEventListener('change', handleRegionChange)
+countryDD.addEventListener('change', handleCountryChange)
 
 async function handleRegionChange() {
     const url = `${regionURL}${regionDD.value}`  //construct the proper URL Route to get the data [endpoint]
     const data = await fetch(url) //download the data
     const countries = await data.json()  //parse the data
     const countriesOptions = countries
-        .map(country => `<option value = ${country.name.common}> ${country.name.common}</option>`)
+        .map(country => `<option value ='${country.name.common}'> ${country.name.common}</option>`)
         .join(' ')
 
     countryDD.innerHTML = countriesOptions
+}
+async function handleCountryChange() {
+    const url = `${countryURL}${countryDD.value}`  //construct the proper URL Route to get the data [endpoint]
+    const data = await fetch(url) //download the data
+    const countries = await data.json()  //parse the data
+    const country = countries[0]
+
+    factsArea.innerHTML = `
+        <h2>${country.name.official}</h2>
+        <img src="${country.flags.png}" alt="The country of ${country.name.official}">
+
+        <table>
+        <tr>
+            <th>Official Country Name</th>
+            <td>${country.name.official}</td>
+        </tr>
+        </table>
+    `
 }

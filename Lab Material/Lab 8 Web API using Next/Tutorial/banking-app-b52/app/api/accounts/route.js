ba97b1ae1e4ce317fc083befb241df16a8fd5114
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-20 12:05:40 +0300

    updated

--- Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b52/app/api/accounts/route.js
+++ Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b52/app/api/accounts/route.js
@@ -1,10 +1,13 @@
+import AccountsRepo from "@/app/repo/accounts-repo"
+const accountsRepo = new AccountsRepo()
 
 export async function GET(request) {
+    const accounts = await accountsRepo.getAccounts()
     return Response.json(accounts, { status: 200 })
 }
 export async function POST(request) {
     const account = await request.json()
-   
+
     return Response.json(account, { status: 201 })
 }
 
--- Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b52/app/repo/accounts-repo.js
+++ Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b52/app/repo/accounts-repo.js
@@ -1,45 +1,96 @@
-class AccountsRepo {
+// npm i fs-extra
+import fs from 'fs-extra'
+import { nanoid } from 'nanoid'
+import path from 'path'
+
+export default class AccountsRepo {
     constructor() {
-        this.accounts = [
-            {
-                accountNo: 123,
-                balance: 1000,
-                accountType: "Savings"
-            },
-            {
-                accountNo: 124,
-                balance: 2000,
-                accountType: "Current"
-            }
-        ]
-
+        this.path = path.join(process.cwd(), 'app/data/accounts.json')
+        console.log(this.path);
+    }
+    // This can be used if we want to generate a unique account number, but the account no should be a number
+    // otherwise use nano id which generates a unique id but string 
+    async generateUniqueAccountNo() {
+        const accounts = await this.getAccounts()
+        return Math.max(...accounts.map(account => account.accountNo)(...accountNumbers)) + 1
     }
 
-    getAccounts() {
-        return this.accounts;
-    }
+    async getAccounts(type) {
+        const accounts = await fs.readJSON(this.path)
+        if (type) {
+            return accounts.filter(account => account
+                .acctType.toLowerCase() === type.toLowerCase())
+        }
 
-    addAccount(account) {
-        account.accountNo = Math.floor(Math.random() * 1000) + 1
+        return accounts;
+    }
+    async addAccount(account) {
+        account.accountNo = nanoid()
+        const accounts = await this.getAccounts()
         accounts.push(account)
+        await fs.writeJSON(this.path, accounts)
         return account
     }
 
-    updateAccount(account) {
-        const index = accounts.findIndex(acc => acc.accountNo === account.accountNo)
-        if (index === -1)
-            return "account not found"
-
-        accounts[index] = account
-        return "successfully updated account"
+    async updateAccount(account, accountNo) {
+        const accounts = await fs.readJson(this.path)
+        const index = accounts.findIndex(acc => acc.accountNo == accountNo)
+        console.log(index);
+        if (index >= 0) {
+            accounts[index] = { ...accounts[index], ...account }
+            await fs.writeJson(this.path, accounts)
+            return "updated successfully"
+        }
+        return "Unable to update account because it does not exist"
     }
 
-    deleteAccount(accountNo){
-        const index = accounts.findIndex(acc => acc.accountNo === accountNo)
-        if (index === -1)
-            return "account not found"
+    async getAccount(accNo) {
+        const accounts = await fs.readJson(this.path)
+        const index = accounts.findIndex(acc => acc.accountNo == accNo)
+        if (index >= 0) {
+            return accounts[index]
+        }
+        return "Account not found"
+    }
+
+    async deleteAccount(accNo) {
+        const accounts = await fs.readJson(this.path)
+        const index = accounts.findIndex(acc => acc.accountNo == accNo)
+        if (index < 0) return "Account not found"
 
         accounts.splice(index, 1)
-        return "successfully deleted account" 
+        await fs.writeJson(this.path, accounts)
+        return "deleted successfully"
+    }
+    async getTransactions(accountNo) {
+        const accounts = await this.getAccounts();
+        const account = accounts.find(account => account.accountNo == accountNo);
+        if (!account)
+            return "Account not found";
+
+        else if (!account.transactions)
+            return "No transactions found";
+
+        return account.transactions;
+    }
+    async addTransaction(transaction, accountNo) {
+        console.log('I was called');
+        transaction.accountNo = accountNo
+        transaction.amount = parseInt(transaction.amount.toString());
+        try {
+            const accounts = await this.getAccounts();
+            const account = accounts.find(account => account.accountNo == accountNo);
+            if (transaction.transType == 'Deposit')
+                account.balance += transaction.amount;
+            else if (transaction.transType == 'Withdraw')
+                if (account.balance < transaction.amount)
+                    return "Insufficient balance";
+                else
+                    account.balance -= transaction.amount;
+            await fs.writeJSON(this.path, accounts)
+            return account
+        } catch (err) {
+            throw err;
+        }
     }
 }

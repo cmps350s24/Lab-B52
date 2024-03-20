52103637dc7b0e176aa10bb8417d4ac0c01e0c47
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-03-20 11:38:25 +0300

    updated

--- Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b52/app/api/accounts/route.js
+++ Lab Material/Lab 8 Web API using Next/Tutorial/banking-app-b52/app/api/accounts/route.js
@@ -18,5 +18,18 @@ export async function POST(request) {
     const account = await request.json()
     account.accountNo = Math.floor(Math.random() * 1000) + 1
     accounts.push(account)
-    return Response.json(account, { status: 200 })
+    return Response.json(account, { status: 201 })
 }
+
+export async function PUT(request) {
+    const account = await request.json()
+    const index = accounts.findIndex(acc => acc.accountNo === account.accountNo)
+    if (index === -1)
+        return Response.json({ message: "account not found" }, { status: 404 })
+
+    accounts[index] = account
+    return Response.json({ message: "successfully updated" }, { status: 200 })
+}
+
+// npm i
+// npm run dev

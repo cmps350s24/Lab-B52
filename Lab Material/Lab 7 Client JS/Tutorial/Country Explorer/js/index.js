6b43472510fa14c89596c1b7818f00a1716c61f6
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-04-03 10:16:50 +0300

    up lab 9

--- Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
+++ Lab Material/Lab 7 Client JS/Tutorial/Country Explorer/js/index.js
@@ -1,62 +1,62 @@
-// const regionURL = 'https://restcountries.com/v3.1/region/'
-// const countryURL = 'https://restcountries.com/v3.1/name/'
+const regionURL = 'https://restcountries.com/v3.1/region/'
+const countryURL = 'https://restcountries.com/v3.1/name/'
 
-// // Get the reference of the element we want to interact with
-// const regionDD = document.querySelector('#region')
-// const countryDD = document.querySelector('#country')
-// const factsArea = document.querySelector('#facts-area')
+// Get the reference of the element we want to interact with
+const regionDD = document.querySelector('#region')
+const countryDD = document.querySelector('#country')
+const factsArea = document.querySelector('#facts-area')
 
-// // add event listener to the selected element
+// add event listener to the selected element
 
-// regionDD.addEventListener('change', handleRegionChange)
-// countryDD.addEventListener('change', handleCountryChange)
+regionDD.addEventListener('change', handleRegionChange)
+countryDD.addEventListener('change', handleCountryChange)
 
-// async function handleRegionChange() {
-//     const url = `${regionURL}${regionDD.value}`  //construct the proper URL Route to get the data [endpoint]
-//     const data = await fetch(url) //download the data
-//     const countries = await data.json()  //parse the data
-//     const countriesOptions = countries
-//         .map(country => `<option value ='${country.name.common}'> ${country.name.common}</option>`)
-//         .join(' ')
+async function handleRegionChange() {
+    const url = `${regionURL}${regionDD.value}`  //construct the proper URL Route to get the data [endpoint]
+    const data = await fetch(url) //download the data
+    const countries = await data.json()  //parse the data
+    const countriesOptions = countries
+        .map(country => `<option value ='${country.name.common}'> ${country.name.common}</option>`)
+        .join(' ')
 
-//     countryDD.innerHTML = countriesOptions
-// }
-// async function handleCountryChange() {
-//     const url = `${countryURL}${countryDD.value}`  //construct the proper URL Route to get the data [endpoint]
-//     const data = await fetch(url) //download the data
-//     const countries = await data.json()  //parse the data
-//     const country = countries[0]
+    countryDD.innerHTML = countriesOptions
+}
+async function handleCountryChange() {
+    const url = `${countryURL}${countryDD.value}`  //construct the proper URL Route to get the data [endpoint]
+    const data = await fetch(url) //download the data
+    const countries = await data.json()  //parse the data
+    const country = countries[0]
 
-//     const currKeys = Object.keys(country.currencies)
-//     const langKeys = Object.keys(country.languages)
+    const currKeys = Object.keys(country.currencies)
+    const langKeys = Object.keys(country.languages)
 
-//     const currencies = currKeys.map(key => country.currencies[key].name).join(' - ')
-//     const languages = langKeys.map(key => country.languages[key]).join(' - ')
+    const currencies = currKeys.map(key => country.currencies[key].name).join(' - ')
+    const languages = langKeys.map(key => country.languages[key]).join(' - ')
 
-//     factsArea.innerHTML = `
-//         <h2>${country.name.official}</h2>
-//         <img src="${country.flags.png}" alt="${country.flags.alt}">
+    factsArea.innerHTML = `
+        <h2>${country.name.official}</h2>
+        <img src="${country.flags.png}" alt="${country.flags.alt}">
 
-//         <table>
-//             <tr>
-//                 <th>Official Country Name</th>
-//                 <td>${country.name.official}</td>
-//             </tr>
-//             <tr>
-//                 <th>Population</th>
-//                 <td>${country.population}</td>
-//             </tr>
-//             <tr>
-//                 <th>Currencies</th>
-//                 <td>${currencies}</td>
-//             </tr>
-//             <tr>
-//                 <th>Languages</th>
-//                 <td>${languages}</td>
-//             </tr>
-//         </table>
-//     `
-// }
+        <table>
+            <tr>
+                <th>Official Country Name</th>
+                <td>${country.name.official}</td>
+            </tr>
+            <tr>
+                <th>Population</th>
+                <td>${country.population}</td>
+            </tr>
+            <tr>
+                <th>Currencies</th>
+                <td>${currencies}</td>
+            </tr>
+            <tr>
+                <th>Languages</th>
+                <td>${languages}</td>
+            </tr>
+        </table>
+    `
+}
 
 
 

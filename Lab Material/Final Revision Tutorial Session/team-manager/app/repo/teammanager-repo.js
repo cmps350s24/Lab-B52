f9e31b2c9710bb1c563126353b15bf82bb74a8e9
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-05-01 11:30:57 +0300

    upto the repo

---
+++ Lab Material/Final Revision Tutorial Session/team-manager/app/repo/teammanager-repo.js
@@ -0,0 +1,76 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+class TeamManagerRepo {
+    async addTeam(team) {
+        try {
+
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async addPlayer(player) {
+        try {
+            return prisma.player.create({ data: player })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async updateTeam(id, team) {
+        try {
+            return prisma.team.update(
+                {
+                    data: team,
+                    where: { id }
+                }
+            )
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+    async updatePlayer(id, player) {
+        try {
+            return prisma.player.update(
+                {
+                    data: player,
+                    where: { id }
+                }
+            )
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async deleteTeam(id) {
+        try {
+            return prisma.team.delete({ where: { id: id } })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+    async deletePlayer(id) {
+        try {
+            return prisma.player.delete({ where: { id: id } })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+    async getTeams() {
+        try {
+            return prisma.team.findMany()
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+    async getTeamPlayers(teamId) {
+        try {
+            return prisma.player.findMany({
+                where: { teamId: teamId }
+            })
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+}
